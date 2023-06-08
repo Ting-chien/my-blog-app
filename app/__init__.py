9935082529e8e2072acb2987e5b1f42dc3d937cb
Eric Wang <eric.wang@theomnieyes.com>
2023-06-08 18:06:31 +0800

    Add app init function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,35 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from importlib import import_module
+
+db = SQLAlchemy()
+
+
+def register_blueprints(app):
+    for module_name in ["base"]:
+        module = import_module('app.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+
+def configure_database(app):
+
+    @app.before_first_request
+    def initialize_database():
+        db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove()
 
 
 def create_app(config):
 
     app = Flask(__name__)
     app.config.from_object(config)
+
+    db.init_app(app)
+    register_blueprints(app)
+    configure_database(app)
 
     @app.route('/')
     def index():
