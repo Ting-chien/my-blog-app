8987e90e9d5d5cda9a1cc3713a571b95cf73a942
ting-chien <eric821210@gmail.com>
2024-11-09 17:35:48 +0800

    Add edit personal profile feature

--- app/base/routes.py
+++ app/base/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 
 from . import blueprint
+from .forms import EditProfileForm
+from .. import db
 from ..decorators import admin_required, permission_required
 from ..auth.models import Permission, User
 
@@ -17,6 +19,27 @@ def user(username):
     # 透過username來向資料庫查詢符合的用戶，入查無使用者則返回404頁面
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@blueprint.route("/edit-profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    """編輯個人資訊頁的資訊"""
+    form = EditProfileForm()
+    # 若有送出表單，則會更新個人資訊，並從新跳轉到此頁
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    # 若表單位送出，則會帶入current_user的資訊
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
 
 
 @blueprint.route("/admin")
