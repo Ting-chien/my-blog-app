22c4d835b74e341c3107238966144d18f57ed217
ting-chien <eric821210@gmail.com>
2024-11-09 18:16:38 +0800

    Add edit admin profile feature

--- app/base/forms.py
+++ app/base/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms import ValidationError
+
+from ..auth.models import User, Role
 
 
 class NameForm(FlaskForm):
@@ -14,3 +17,42 @@ class EditProfileForm(FlaskForm):
     location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(FlaskForm):
+    # EditProfileAdminForm 比 EditProfileForm 多了 email, username, confirmed, 
+    # role 等欄位供管理者編輯
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username', validators=[
+        DataRequired(), Length(1, 64),
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+               'Usernames must have only letters, numbers, dots or '
+               'underscores')])
+    confirmed = BooleanField('Confirmed')
+    # 像是 RadioField, SelectField, SelectMultipleField 等輸入匡，回提供一變數
+    # choice 來傳入選項，其結構為 List[(編號, 顯示名稱)]，並且可透過另一變數 coerce 
+    # 來指定編號的資料型態
+    role = SelectField('Role', coerce=int) 
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        # 在這裡組裝角色輸入匡的選項
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        # 在這裡保存路由傳過來要被更改的 user
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
